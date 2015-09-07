13d765c1b412197a5ae780669bc755fe7d87dace
auni53 <auni.ahsan@mail.utoronto.ca>
2015-09-06 20:25:56 -0400

    index.html prints the selected option; data.json contains all raw data in a json format

--- process.py
+++ process.py
@@ -1,15 +1,19 @@
-import csv
+import json
 
 def do(filename):
     with open(filename) as file:
         data = {}
         raw = file.readlines()
-        for line in raw[1:]:
+        for line in raw[1:-1]:
             items = line.strip().split(",")
-            colour = items[0]
-            data[colour] = items[1:]
+            if items[0] != "":
+                colour = items[0]
+                data[colour] = items[1:-1]
     return data
  
 if __name__ == "__main__":
-    for day in ["monday.csv", "tuesday.csv", "wednesday.csv", "thursday.csv", "friday.csv"]:
-        print(do(day))
+    with open("data.json", "w") as file:
+        for day in ["monday", "tuesday", "wednesday", "thursday", "friday"]:
+            raw_dict = do((day + ".csv"))
+            file.write(json.dumps(raw_dict))
+            file.write("\n")
