2eb26e3b0f4cbaf0da17df4c1e3f9c93276b6c69
auni53 <auni.ahsan@mail.utoronto.ca>
2016-09-03 22:01:53 -0400

    updated for 2016

--- process.py
+++ process.py
@@ -4,16 +4,24 @@ def do(filename):
     with open(filename) as file:
         data = {}
         raw = file.readlines()
-        for line in raw[1:-1]:
+        for line in raw[1:]:
             items = line.strip().split(",")
             if items[0] != "":
-                colour = items[0]
-                data[colour] = items[1:-1]
+                colour = items[0].lower()
+
+                c = 0
+                for event in items:
+                    if (c > 0 and event == ''):
+                        items[c] = items[c - 1]
+                    c += 1
+
+                data[colour] = items[1:]
     return data
  
 if __name__ == "__main__":
     with open("data.json", "w") as file:
-        for day in ["monday", "tuesday", "wednesday", "thursday", "friday"]:
+        for day in ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"]:
             raw_dict = do((day + ".csv"))
+            file.write(day[0:3] + ": ")
             file.write(json.dumps(raw_dict))
-            file.write("\n")
+            file.write(",\n")
